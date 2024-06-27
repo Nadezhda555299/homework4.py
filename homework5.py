171abbcd0d7fb6781ce411a172e8c64a5e2c8bac
Nadezhda555299 <89114940211@mail.ru>
2024-06-27 19:43:35 +0200

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+# immutable_var = 2, 4, 6, int, 'Student'
+# print(immutable_var)
+# immutable_var[2] = 400
+# print(immutable_var)
+
+mutable_list = ([3, 4, 5,], 6, 7)
+print(mutable_list)
+mutable_list[0][0] = 5
+print(mutable_list)
