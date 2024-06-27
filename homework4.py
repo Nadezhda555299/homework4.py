f637f870f9a6a7cee73c72a4bd39782791b7f68c
Nadezhda555299 <89114940211@mail.ru>
2024-06-27 19:40:22 +0200

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+my_string = input('Как называется твой город: ')
+# my_string = 'Как называется твой город'
+# print(len(my_string))
+print(input())
+print(my_string.upper())
+print(my_string.upper().lower())
+print('Как называется твой город?'. replace(' ', ''))
+example = 'Как называется твой город?'
+print(example[0])
+print(example[-1])
